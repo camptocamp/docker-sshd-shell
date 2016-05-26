dcc2a28d7ca68aba0f8c4c098a092655a652a5e0
Marc Fournier <marc.fournier@camptocamp.com>
2016-05-26 15:20:08 +0200

    make entrypoint scripts idempotent

--- docker-entrypoint.d/05-ssh_host_keys.sh
+++ docker-entrypoint.d/05-ssh_host_keys.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -e
 
 for format in rsa dsa ecdsa ed25519; do
+  test -f "/etc/ssh/ssh_host_${format}_key" && continue
   ssh-keygen -f "/etc/ssh/ssh_host_${format}_key" -N '' -t $format
   ssh-keygen -l -f "/etc/ssh/ssh_host_${format}_key.pub"
 done
--- docker-entrypoint.d/10-users.sh
+++ docker-entrypoint.d/10-users.sh
@@ -4,7 +4,7 @@ test -n "$USERLIST" || echo 'Not adding any users. You can pass a list of users 
 
 for user in $USERLIST; do
   echo "adding user $user"
-  useradd --shell /bin/bash --create-home --user-group --no-log-init "${user}"
-  mkdir "/home/${user}/.ssh/"
+  getent passwd "${user}" > /dev/null || useradd --shell /bin/bash --create-home --user-group --no-log-init "${user}"
+  mkdir -p "/home/${user}/.ssh/"
   curl --max-time 15 -s "https://github.com/${user}.keys" > "/home/${user}/.ssh/authorized_keys"
 done
